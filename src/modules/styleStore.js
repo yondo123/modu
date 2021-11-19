458a3b46776042879b0dc64899284e46afa897a6
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-11-19 23:46:14 +0900

    update : vuex 라이브러리 추가

---
+++ src/modules/styleStore.js
@@ -0,0 +1,14 @@
+const state = {
+    mobile: window.innerWidth < 1024 ? true : false
+};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+};
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex';
+import style from '../modules/styleStore';
+
+export const store = new Vuex.Store({
+    modules: {
+        style
+    }
+});
