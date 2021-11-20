309f3551d1cab4be56fadda6ca3afb2fa62d3520
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-11-20 23:58:29 +0900

    update : styleStore 네이밍 변경 (common store)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VCalendar from 'v-calendar';
 import Router from './router';
 import Vuex from 'vuex';
 import {store} from './store/store';
+
 import 'tailwindcss/tailwind.css';
 import './assets/css/global.css';
 import './assets/css/style.css';
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex';
-import style from '../modules/styleStore';
+import style from '../modules/commonStore';
 
 export const store = new Vuex.Store({
     modules: {
