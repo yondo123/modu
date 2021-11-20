495ba8a682495f7d258e4a5f7f4cb77b15d71607
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-11-20 23:58:52 +0900

    update : 사용자 프로필 영역 모듈화

--- src/components/MDProfile.vue
+++ src/components/MDProfile.vue
@@ -1,16 +1,12 @@
 <template>
-    <div class="right-wrap md:col-span=9 lg:col-span-10">
-        <main>
-            <!-- signed -> 로그인, unsign -> 비 로그인 -->
-            <div class="top-wrap unsign">
-                <h2 class="board-title"><span>Recruitment for study.</span></h2>
-                <button type="button" class="login-button">Github Login</button>
-                <div class="user-info hide">
-                    <span><strong>admin</strong>님, 안녕하세요.</span>
-                    <button type="button" class="logout-button">Logout</button>
-                </div>
-            </div>
-        </main>
+    <!-- signed -> 로그인, unsign -> 비 로그인 -->
+    <div class="top-wrap unsign">
+        <h2 class="board-title"><span>Recruitment for study.</span></h2>
+        <button type="button" class="green-button">Github Login</button>
+        <div class="user-info hide">
+            <span><strong>admin</strong>님, 안녕하세요.</span>
+            <button type="button" class="logout-button">Logout</button>
+        </div>
     </div>
 </template>
 
