5fb630db3987814014cda50702a9f62c1ffb59b3
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-11-20 23:59:22 +0900

    update : 버튼 클래스 이름 변경 (재사용)

--- src/components/MDCalendar.vue
+++ src/components/MDCalendar.vue
@@ -4,7 +4,7 @@
             <!-- signed -> 로그인, unsign -> 비 로그인 -->
             <div class="top-wrap unsign">
                 <h2 class="board-title"><span>Recruitment for study.</span></h2>
-                <button type="button" class="login-button">Github Login</button>
+                <button type="button" class="green-button">Github Login</button>
                 <div class="user-info hide">
                     <span><strong>admin</strong>님, 안녕하세요.</span>
                     <button type="button" class="logout-button">Logout</button>
