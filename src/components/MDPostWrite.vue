6f3fb54495b974ebd43311c688d0a9f9b21014b7
yondo123 <46988995+yondo123@users.noreply.github.com>
2021-11-22 21:58:33 +0900

    update : toast-editor 플러그인 추가

--- src/components/MDPostWrite.vue
+++ src/components/MDPostWrite.vue
@@ -1,9 +1,48 @@
 <template>
-    <h2>MDPostWrite.vue</h2>
+    <div class="right-wrap md:col-span=9 lg:col-span-10">
+        <main>
+            <!-- signed -> 로그인, unsign -> 비 로그인 -->
+            <MDProfile></MDProfile>
+            <div class="post-header">
+                <select name="" id="">
+                    <option value="free">Q&A</option>
+                </select>
+            </div>
+            <div ref="edtior" class="editor-wrap"></div>
+        </main>
+    </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup>
+import '@toast-ui/editor/dist/toastui-editor.css';
+import Editor from '@toast-ui/editor';
+import {ref, onMounted} from 'vue';
 
-<style></style>
+const edtior = ref(null);
+
+onMounted(() => {
+    console.log('onMounted');
+
+    console.log('====' + edtior.value);
+    const toastEditor = new Editor({
+        el: edtior.value,
+        height: '720px',
+        initialEditType: 'wysiwyg',
+        previewStyle: 'vertical'
+    });
+    toastEditor.getMarkdown();
+});
+</script>
+<style>
+@import url('../assets/css/board.css');
+.editor-wrap {
+    margin-top: 2.5rem;
+}
+
+/* desktop */
+@media screen and (min-width: 1280px) {
+    .editor-wrap {
+        margin-top: 1.5rem;
+    }
+}
+</style>
