a2b4010ee42b5599cea30494306d3d521c43e8f1
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2021-11-24 17:56:55 +0900

    update : 글 상세 페이지 - 제목, 작성자 영역 구현

--- src/components/MDPost.vue
+++ src/components/MDPost.vue
@@ -1,9 +1,83 @@
 <template>
-    <h2>MDPost.vue</h2>
+    <div class="right-wrap md:col-span=9 lg:col-span-10">
+        <main>
+            <MDProfile></MDProfile>
+            <div class="post-wrap">
+                <h2 class="post-title">웹개발 스터디원 구합니다.웹개발 스터디원 구합니다.</h2>
+                <div class="post-info">
+                    <div class="profile">
+                        <img src="../assets/logo.jpg" alt="user_id" />
+                    </div>
+                    <p class="publish"><span class="writer">admin</span><span class="date">2021. 11. 09. 13:49</span></p>
+                </div>
+            </div>
+            <div class="comment-wrap"></div>
+        </main>
+    </div>
 </template>
 
 <script>
-export default {};
+import MDProfile from '../components/MDProfile.vue';
+export default {
+    components: {
+        MDProfile
+    }
+};
 </script>
 
-<style></style>
+<style scoped>
+@import url('../assets/css/board.css');
+
+.post-wrap {
+    margin-top: 1.5rem;
+    padding: 1.6rem 1.6rem;
+    border: 0.1rem solid var(--border-color);
+    border-radius: 0.8rem;
+}
+
+.post-title {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.post-info {
+    margin-top: 1.5rem;
+    display: flex;
+    position: relative;
+}
+
+.publish {
+    position: absolute;
+    margin-left: 0.8rem;
+    top: 50%;
+    left: 3rem;
+    transform: translateY(-50%);
+}
+
+.writer,
+.date {
+    font-size: 1.2rem;
+    color: var(--font-color);
+}
+
+.date {
+    margin-left: 0.5rem;
+}
+
+/* laptop */
+@media screen and (min-width: 1024px) {
+    .writer,
+    .date {
+        margin-left: 1.5rem;
+    }
+}
+
+/* desktop */
+@media screen and (min-width: 1280px) {
+}
+
+/* desktop-xl */
+@media screen and (min-width: 1536px) {
+}
+</style>
